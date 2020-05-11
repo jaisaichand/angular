61c4cf42a8f86bb9b3e81b4d655328709a0ccc76
JAI <t.jaisaichand4@gmail.com>
2020-05-11 18:01:30 +0530

    ...

--- JPRestaurentMat/src/app/cart/delivery/delivery.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-delivery',
-    templateUrl: 'delivery.component.html'
-})
-export class DeliveryComponent {}
-
-// Solely developed by jai sai chand(t.jaisaichand4@gmail.com)
--- JPRestaurentMat/src/app/cart/payment/payment.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-payment',
-    templateUrl: 'payment.component.html'
-})
-export class PaymentComponent {}
